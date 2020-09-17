a69dc408fea3253f73b8a52f15aa29dbd718cd18
arsenypoga <semiretoja@gmail.com>
2020-09-17 16:17:17 -0400

    Initial Commit

---
+++ src/csc439team6/blackjack/Main.java
@@ -0,0 +1,15 @@
+package csc439team6.blackjack;
+
+/**
+ * @author Arseny Poga
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * @param args blah
+     */
+    public static void main(String[] args) {
+        System.out.println("Working");
+    }
+}
