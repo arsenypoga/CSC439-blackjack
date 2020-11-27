6056182fbbc92b1e994a0aef3736f1d5626e94f8
arsenypoga <semiretoja@gmail.com>
2020-11-26 21:32:23 -0500

    Added logging statements to the AbstractPlayer

--- src/csc439team6/blackjack/models/AbstractPlayer.java
+++ src/csc439team6/blackjack/models/AbstractPlayer.java
@@ -1,11 +1,16 @@
 package csc439team6.blackjack.models;
 
+import csc439team6.blackjack.controllers.MainController;
+
+import java.util.logging.Logger;
+
 /**
  * @author Arseny Poga
  * @version 1.0
  */
 public abstract class AbstractPlayer {
     private final Hand hand;
+    private final Logger logger = Logger.getLogger(MainController.class.getName());
 
     /**
      * Protected constructor to prevent instantiation of a base player class.
@@ -19,7 +24,9 @@ public abstract class AbstractPlayer {
      * @return Hand
      */
     public Hand getHand() {
-        return hand;
+        logger.entering(getClass().getName(), "getHand");
+        logger.exiting(getClass().getName(), "getHand");
+         return hand;
     }
 
     /**
@@ -27,13 +34,18 @@ public abstract class AbstractPlayer {
      * @param card Card
      */
     public void addCard(Card card) {
+        logger.entering(getClass().getName(), "addCard");
         this.hand.addCard(card);
+        logger.exiting(getClass().getName(), "addCard");
     }
 
     /**
      * Resets hand
      */
     public void resetHand() {
+        logger.entering(getClass().getName(), "resetHand");
+
         this.hand.clear();
+        logger.exiting(getClass().getName(), "resetHand");
     }
 }
