c5c314f5c1fd509a5dd188caad23d351b625ffad
arsenypoga <semiretoja@gmail.com>
2020-11-26 21:24:05 -0500

    Added logging statements to the MainController

--- src/csc439team6/blackjack/controllers/MainController.java
+++ src/csc439team6/blackjack/controllers/MainController.java
@@ -21,21 +21,21 @@ public class MainController {
     private final Logger logger = Logger.getLogger(MainController.class.getName());
 
     public MainController(AbstractView view) {
-        logger.entering(getClass().getName(), "methodname");
+        logger.entering(getClass().getName(), "MainController");
 
         this.view = view;
         this.dealer = new Dealer();
         this.shoe =  new Shoe(3);
         this.player = new Player(0);
 
-        logger.exiting(getClass().getName(), "methodname");
+        logger.exiting(getClass().getName(), "MainController");
     }
 
     /**
      * Starts game sequence
      */
     public void playBlackjack() {
-        logger.entering(getClass().getName(), "methodname");
+        logger.entering(getClass().getName(), "playBlackjack");
 
         view.gameStartedMessage();
         purchaseChips();
@@ -50,7 +50,7 @@ public class MainController {
         displayHand(player);
         displayHand(dealer);
 
-        logger.exiting(getClass().getName(), "methodname");
+        logger.exiting(getClass().getName(), "playBlackjack");
     }
 
     /**
@@ -58,7 +58,7 @@ public class MainController {
      *
      */
     public void purchaseChips() {
-        logger.entering(getClass().getName(), "methodname");
+        logger.entering(getClass().getName(), "purchaseChips");
 
         try {
             int chips = view.purchaseChips();
@@ -66,9 +66,11 @@ public class MainController {
             logger.log(Level.INFO,  "Player purchased {} chips", chips);
         } catch (IOException e) {
             view.quitGame();
+
+            logger.exiting(getClass().getName(), "incrementBet");
             System.exit(0);
         }
-        logger.exiting(getClass().getName(), "methodname");
+        logger.exiting(getClass().getName(), "purchaseChips");
     }
 
     /**
@@ -76,15 +78,21 @@ public class MainController {
      * if input is incorrect the request is repeated.
      */
     public void makeInitialBet() {
-        logger.exiting(getClass().getName(), "methodname");
+        logger.entering(getClass().getName(), "makeInitialBet");
         try {
             int bet = view.getInitialBet();
             player.reduceChips(bet);
             player.setBet(bet);
         } catch (IOException e) {
+
+            logger.info("Quit message command received!");
+
             view.quitGame();
+
+            logger.exiting(getClass().getName(), "makeInitialBet");
             System.exit(0);
         }
+        logger.exiting(getClass().getName(), "makeInitialBet");
     }
 
     /**
@@ -92,7 +100,9 @@ public class MainController {
      * @param player player
      */
     public void displayHand(AbstractPlayer player) {
+        logger.entering(getClass().getName(), "displayHand");
         view.displayHand(player);
+        logger.exiting(getClass().getName(), "displayHand");
     }
 
     /**
@@ -101,22 +111,32 @@ public class MainController {
      * @param player player
      */
     public void dealCard(AbstractPlayer player) {
+        logger.entering(getClass().getName(), "dealCard");
+
         Card card = shoe.pickCard();
+        logger.log(Level.INFO, "Player received {0}", card);
         player.addCard(card);
+
+        logger.exiting(getClass().getName(), "dealCard");
     }
 
     /**
      * Increments bet
      */
     public void incrementBet() {
+        logger.entering(getClass().getName(), "incrementBet");
+
         try {
-            int bet = +view.incrementBet();
+            int bet = view.incrementBet();
             player.reduceChips(bet);
 
         } catch (IOException e) {
             view.quitGame();
+
+            logger.exiting(getClass().getName(), "incrementBet");
             System.exit(0);
         }
+        logger.exiting(getClass().getName(), "incrementBet");
     }
 
     /**
@@ -124,7 +144,10 @@ public class MainController {
      * @return view
      */
     public AbstractView getView() {
+        logger.entering(getClass().getName(), "getView");
+        logger.exiting(getClass().getName(), "getView");
         return view;
+
     }
 
     /**
@@ -132,7 +155,11 @@ public class MainController {
      * @param view
      */
     public void setView(AbstractView view) {
+        logger.entering(getClass().getName(), "setView");
+
         this.view = view;
+
+        logger.exiting(getClass().getName(), "setView");
     }
 
     /**
@@ -140,6 +167,8 @@ public class MainController {
      * @return player
      */
     public Player getPlayer() {
+        logger.entering(getClass().getName(), "getPlayer");
+        logger.exiting(getClass().getName(), "getPlayer");
         return player;
     }
 
@@ -148,7 +177,10 @@ public class MainController {
      * @param player
      */
     public void setPlayer(Player player) {
+        logger.entering(getClass().getName(), "getPlayer");
         this.player = player;
+        logger.entering(getClass().getName(), "getPlayer");
+
     }
 
     /**
@@ -156,6 +188,8 @@ public class MainController {
      * @return
      */
     public Dealer getDealer() {
+        logger.entering(getClass().getName(), "getDealer");
+        logger.exiting(getClass().getName(), "getDealer");
         return dealer;
     }
 
@@ -164,7 +198,9 @@ public class MainController {
      * @param dealer
      */
     public void setDealer(Dealer dealer) {
+        logger.entering(getClass().getName(), "setDealer");
         this.dealer = dealer;
+        logger.exiting(getClass().getName(), "setDealer");
     }
 
     /**
@@ -172,6 +208,8 @@ public class MainController {
      * @return
      */
     public Shoe getShoe() {
+        logger.entering(getClass().getName(), "getShoe");
+        logger.exiting(getClass().getName(), "getShoe");
         return shoe;
     }
 
@@ -180,6 +218,9 @@ public class MainController {
      * @param shoe
      */
     public void setShoe(Shoe shoe) {
+        logger.entering(getClass().getName(), "setShoe");
         this.shoe = shoe;
+        logger.exiting(getClass().getName(), "setShoe");
+
     }
 }
